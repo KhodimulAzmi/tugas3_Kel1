1278b6dec8c7ff361172420bd46093fba0952035
KhodimulAzmi <168795712+KhodimulAzmi@users.noreply.github.com>
2024-11-21 09:22:11 +0700

    4th

--- routes.php
+++ routes.php
@@ -16,25 +16,25 @@ switch ($controller) {
 
     case 'organizers':
         $organizersController = new OrganizersController();
-       if ($url == '/organizers/index' || $url == '/') {
-    $organizers->index();
-} elseif ($url == '/organizers/create' && $requestMethod == 'GET') {
-    $organizers->create();
-} elseif ($url == '/organizers/store' && $requestMethod == 'POST') {
-    $organizers->store();
-} elseif (preg_match('/\/organizers\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $organizersId = $matches[1];
-    $organizers->edit($organizersId);
-} elseif (preg_match('/\/organizers\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $organizersId = $matches[1];
-    $organizers->update($organizersId, $_POST);
-} elseif (preg_match('/\/organizers\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $organizersId = $matches[1];
-    $organizers->delete($organizersId);
-} else {
-    http_response_code(404);
-    echo "404 Not Found";
-}
+        if ($url == '/organizers/index' || $url == '/') {
+            $organizers->index();
+        } elseif ($url == '/organizers/create' && $requestMethod == 'GET') {
+            $organizers->create();
+        } elseif ($url == '/organizers/store' && $requestMethod == 'POST') {
+            $organizers->store();
+        } elseif (preg_match('/\/organizers\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+            $organizersId = $matches[1];
+            $organizers->edit($organizersId);
+        } elseif (preg_match('/\/organizers\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+            $organizersId = $matches[1];
+            $organizers->update($organizersId, $_POST);
+        } elseif (preg_match('/\/organizers\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+            $organizersId = $matches[1];
+            $organizers->delete($organizersId);
+        } else {
+            http_response_code(404);
+            echo "404 Not Found";
+        }
         break;
 
     case 'attendees':
